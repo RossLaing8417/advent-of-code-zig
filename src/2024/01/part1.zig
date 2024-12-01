fc6d479995b678215ac0803a69f59bd1a10fe4aa
Ross Laing <30736276+RossLaing8417@users.noreply.github.com>
2024-12-01 11:15:50 +0200

    2024 day 1

---
+++ src/2024/01/part1.zig
@@ -0,0 +1,57 @@
+const std = @import("std");
+
+pub fn main() !void {
+    const data = @embedFile("input.txt");
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    defer std.debug.assert(gpa.deinit() == .ok);
+    const result = try solve(gpa.allocator(), data);
+    try std.io.getStdOut().writer().print("{d}\n", .{result});
+}
+
+fn solve(allocator: std.mem.Allocator, data: []const u8) !isize {
+    var result: isize = 0;
+
+    var data_itr = std.mem.split(u8, data, "\n");
+    var left_nums = try std.ArrayList(i32).initCapacity(allocator, std.mem.count(u8, data, "\n") + 1);
+    defer left_nums.deinit();
+
+    var right_nums = try std.ArrayList(i32).initCapacity(allocator, left_nums.capacity);
+    defer right_nums.deinit();
+
+    while (data_itr.next()) |line| {
+        if (line.len == 0) {
+            continue;
+        }
+
+        var itr = std.mem.tokenizeScalar(u8, line, ' ');
+        const left = try std.fmt.parseInt(i32, itr.next().?, 10);
+        const right = try std.fmt.parseInt(i32, itr.next().?, 10);
+
+        left_nums.appendAssumeCapacity(left);
+        right_nums.appendAssumeCapacity(right);
+    }
+
+    std.mem.sort(i32, left_nums.items, {}, std.sort.asc(i32));
+    std.mem.sort(i32, right_nums.items, {}, std.sort.asc(i32));
+
+    for (left_nums.items, right_nums.items) |l, r| {
+        result += @abs(l - r);
+    }
+
+    return result;
+}
+
+test "Example" {
+    const data =
+        \\3   4
+        \\4   3
+        \\2   5
+        \\1   3
+        \\3   9
+        \\3   3
+    ;
+
+    const result = try solve(std.testing.allocator, data);
+
+    try std.testing.expectEqual(@as(@TypeOf(result), 11), result);
+}
